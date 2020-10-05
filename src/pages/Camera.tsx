752a36a5a665a6e3136f21b148092d790a4f5a57
Leonardo Santos <leonardo.oliveira@vetta.com.br>
2020-10-05 10:22:54 -0300

    ajuste camera

--- src/pages/Camera.tsx
+++ src/pages/Camera.tsx
@@ -1,12 +1,49 @@
 import { Camera } from 'expo-camera';
-import React from 'react';
-import { View } from 'react-native';
+import { useIsFocused } from '@react-navigation/native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Platform, Alert } from 'react-native';
 
 const CameraView: React.FC = () => {
+  const ifFocused = useIsFocused();
+  const [scanned, setScanned] = useState(false);
+
+  const [hasPermission, setHasPermission] = useState<string | null | boolean>(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    if (Platform.OS === 'web') {
+      setHasPermission(true);
+    } else {
+      (async () => {
+        const { status } = await Camera.requestPermissionsAsync();
+        setHasPermission(status);
+      })();
+    }
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (!hasPermission && hasPermission !== "granted") {
+    return <Text>No access to camera</Text>;
+  }
+
+  const handleBarCodeScanned = ({ data }) => {
+    setScanned(true);
+    Alert.alert('Código', data, [
+      { text: "OK", onPress: () => setScanned(false) }
+    ],
+    { cancelable: false } );
+  };
   
-  return (
+  return ifFocused && (
     <View style={{ flex: 1}}>
-      <Camera style={{ flex: 1}}/>
+      <Camera 
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}  
+        style={{ flex: 1}}
+        type={type}
+        ratio='16:9'
+      />
     </View>
   );
 }
